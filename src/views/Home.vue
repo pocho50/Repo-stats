bce73a9b9ba35baf793ed08da82430aa0d07a3fc
Jose Luis Cabrera <cabrera@inkuba.com>
2022-11-21 00:48:53 -0300

    fix: not show barchart when error

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -35,5 +35,5 @@ const handleSearch = async (newSearch: Search) => {
   <SearchForm :loading="loading" @@search="handleSearch" />
   <ReposResult v-if="activeSearch && !error" :repos="repos" class="mt-10" />
   <Alert v-if="error" :msg="error" :type="'alert-error'" class="mt-5" />
-  <BarChart v-if="repos.length > 0" :repos="repos" :width="400" :height="400" class="mt-10" />
+  <BarChart v-if="repos.length > 0 && !error" :repos="repos" :width="400" :height="400" class="mt-10" />
 </template>
